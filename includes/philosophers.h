2b0a31322a5813fc035bc99890e0d02a4e08c3f6
Martin QUEGUINER <mqueguin@e3r5p8.42.fr>
2021-10-12 17:03:55 +0200

    Debut parsing (On recupere juste sans gros traitement) / Creation struct info et struct philo (struct info pas encore initialiser)

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mqueguin <mqueguin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 11:48:54 by mqueguin          #+#    #+#             */
-/*   Updated: 2021/10/12 16:50:10 by mqueguin         ###   ########.fr       */
+/*   Updated: 2021/10/12 17:02:51 by mqueguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,15 @@ typedef struct	s_info
 	int			time_to_sleep;
 	int			number_of_eat;
 	int			b_number_of_eat;
+	int			timestamp; // Permet de faire une soustraction pour afficher le temps ecouler en miliseconds
 }				t_info;
+
+typedef struct	s_philo
+{
+	int			forks[2]; // forks[0] : fourchette gauche; forks[1] : fourchette droite
+	//Mettre le thread ici
+	pthread_t	thread_philo;
+}				t_philo;
 
 int		ft_isdigit_str(char *str);
 
